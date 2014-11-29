8b1c85093bf9c3d2fdc56aea068fddb8fd06fa89
TYM-laptop <tym1060326@163.com>
2014-11-29 16:27:27 +0800

    I have no idea what I did.

--- compressor.py
+++ compressor.py
@@ -1,1 +1,13 @@
 __author__ = 'TYM'
+
+import ply
+
+def js(file_name, encoding='utf8'):
+    '''opens the file, compress it, and returns the compressed string'''
+    s = ''
+    with open(file_name, encoding=encoding) as f:
+        for l in f:
+            if '//' in l:
+                l = l.split('//')[0].strip()
+            elif '/*' in l:
+
