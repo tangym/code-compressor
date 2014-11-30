beb5704f7f83f219db40132c0d5db255ebdeee91
TYM-laptop <tym1060326@163.com>
2014-12-01 00:13:31 +0800

    modify the configuration file in order to fit the gui

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-SOURCE_DIR = 'D:/PycharmProjects/test/'
-TARGET_DIR = 'D:/PycharmProjects/test_/'
+SOURCE_DIR = ''
+TARGET_DIR = ''
 
 SOURCE_ENCODING = 'utf8'
 TARGET_ENCODING = SOURCE_ENCODING
 
 # 文件扩展名，值可以是列表类型，忽略大小写
 EXTENSION = {
-    'javascript': 'js',
-    'php': 'php',
+    'javascript': ('js',),
+    'php': ('php',),
 }
--- main.py
+++ main.py
@@ -4,26 +4,7 @@ import logging
 import compressor
 import config
 
-if os.path.exists(config.TARGET_DIR):
-    logging.error('target dir already exists.')
-    raise(SystemExit)
-else:
-    os.mkdir(config.TARGET_DIR)
-
-# copy dir structure
-for root, dirs, files in os.walk(config.SOURCE_DIR):
-    if dirs:
-        root = root.replace(config.SOURCE_DIR, config.TARGET_DIR)
-        os.chdir(root)
-        for dir in dirs:
-            os.mkdir(dir)
-
-compressor_map = {
-    'javascript': compressor.JsCompressor,
-    'php': compressor.PhpCompressor
-}
-
-# 将目录下的所有以extension为扩展名的文件，用compress处理并写道对应目录中
+# 将目录下的所有以extension为扩展名的文件，用compress处理并写到对应目录中
 def dispatch(extension, compress):
     for s_root, dirs, files in os.walk(config.SOURCE_DIR):
         if files:
@@ -35,12 +16,32 @@ def dispatch(extension, compress):
                         with open('%s/%s' % (t_root, file), 'w', encoding=config.TARGET_ENCODING) as fout:
                             fout.write(compress(fin.read()))
 
-for key in config.EXTENSION:
-    if key in compressor_map:
-        if type(config.EXTENSION[key]) == type(''):
-            dispatch(config.EXTENSION[key], compressor_map[key].compress)
-        elif type(config.EXTENSION[key]) == type([]):
-            for ext in config.EXTENSION[key]:
-                dispatch(ext, compressor_map[key].compress)
+if __name__ == '__main__':
+    if os.path.exists(config.TARGET_DIR):
+        logging.error('target dir already exists.')
+        raise(SystemExit)
     else:
-        logging.warning('%s compressor not found.' % key)
+        os.mkdir(config.TARGET_DIR)
+
+    # copy dir structure
+    for root, dirs, files in os.walk(config.SOURCE_DIR):
+        if dirs:
+            root = root.replace(config.SOURCE_DIR, config.TARGET_DIR)
+            os.chdir(root)
+            for dir in dirs:
+                os.mkdir(dir)
+
+    compressor_map = {
+        'javascript': compressor.JsCompressor,
+        'php': compressor.PhpCompressor
+    }
+
+    for key in config.EXTENSION:
+        if key in compressor_map:
+            if type(config.EXTENSION[key]) == type(''):
+                dispatch(config.EXTENSION[key], compressor_map[key].compress)
+            elif type(config.EXTENSION[key]) == type([]):
+                for ext in config.EXTENSION[key]:
+                    dispatch(ext, compressor_map[key].compress)
+        else:
+            logging.warning('%s compressor not found.' % key)
